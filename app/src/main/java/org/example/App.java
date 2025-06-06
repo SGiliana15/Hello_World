acad64c1893fe3314fc748fa48523587ab805b4f
Sean <zerog133@yahoo.com>
2025-06-05 23:34:43 -0500

    Added Hello world

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,5 +3,8 @@ package org.example;
 public class App {
   public static void main(String[] args) {
     // Your code goes here
+
+    HelloWorld helloWorld = new HelloWorld();
+    System.out.print(helloWorld.getGreeting());
   }
 }
--- app/src/main/java/org/example/HelloWorld.java
+++ app/src/main/java/org/example/HelloWorld.java
@@ -2,4 +2,8 @@ package org.example;
 
 public class HelloWorld {
   // Your code goes here
+
+  public String getGreeting(){
+    return "Hello World!";
+  }
 }
